abd024b7edfb8d186ed07747084f1d835e7d8eb0
lmc327 <lmc327@cornell.edu>
2018-10-31 15:40:34 -0400

    Vuetifyjson

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
