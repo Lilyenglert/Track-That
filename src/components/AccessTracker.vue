952d71e23155ae6228ed0bd7c87f9e7183c655cd
smb464 <smb464@cornell.edu>
2018-11-12 14:54:58 -0500

    added tracker + entry

--- src/components/AccessTracker.vue
+++ src/components/AccessTracker.vue
@@ -3,7 +3,7 @@
         <button @click="getLocal">Access trackers</button>
         <div v-for="tracker in trackers" v-bind:key="tracker.id">
             <p>{{tracker.name}}</p>
-            <!-- <button @click="remove(n)">Remove</button> -->
+            <button @click="createEntry(tracker.name)">Create Entry</button>
         </div>
        </div> 
 </template>
@@ -15,13 +15,55 @@ export default {
        trackers:[{
          name:'',
          unit:''}
-       ]
+       ],
+
+       entries: [
+       {
+         date: null, 
+         value:null,
+         unit:null}
+        ], 
+       currentTracker:'',
+       currentTrackerName:'',
+       currentTrackerUnits:[],
+       entries:[{
+         date:'',
+         amount: [],
+         image:[], 
+       }]
     }
   },
   methods:{
   getLocal()
   {
     this.trackers = JSON.parse(localStorage.getItem('trackers'));
+  },
+  createEntry(trackerName)
+  {
+    console.log('tracker name ' + trackerName);
+    for (let index = 0; index < this.trackers.length; index++) {
+      if(this.trackers[index].name == trackerName)
+      {
+        this.currentTracker = this.trackers[index];
+        this.currentTrackerName = this.trackers[index].name; 
+        this.currentTrackerUnits = this.trackers[index].unit;
+      }
+    }
+
+    var newEntryInput = {
+        "date" : new Date,
+        "value": 7,
+        "unit": this.currentTrackerUnits
+    };
+  this.entries.push(newEntryInput);
+  console.log("pushed");
+
+  const parsed = JSON.stringify(this.entries);
+  localStorage.setItem('entries', parsed);
+
+  // console.log(this.currentTracker);
+  // console.log(this.currentTrackerName);
+  // console.log(this.currentTrackerUnits);
   }
   }
 }
