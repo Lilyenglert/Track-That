26273ee3f3fcbf80fdb7ae8e5372b38a0b84bc99
stephmarky <smarki340@gmail.com>
2018-11-13 20:35:54 -0500

    homepage stuff

--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -9,44 +9,30 @@
         <h1 class='inline-block'>Trackers</h1>
         <p class='inline-block right'><i>edit</i></p>
       </div>
-      <v-container grid-list-lg text-xs-center>
-        <v-layout row wrap fill-height>
-          <v-flex xs6>
-            <v-card color="blue darken-1" class="white--text">
-              <v-card-title primary-title>
-                <div>
-                  <div class="headline">Coffee Budget</div>
-                  <!-- <div>Foster the People</div>
-                  <div>(2014)</div> -->
-                </div>
-              </v-card-title>
-            </v-card>
-          </v-flex>
-          <v-flex xs6>
-            <v-card dark color="primary">
-              <v-card-text class="px-0">Create new</v-card-text>
-            </v-card>
-          </v-flex>
-          
-        </v-layout>
-      </v-container>
 
-
+      <!-- vanilla html/css -->
       <div class='box-container'>
         <div class='box'>
           <router-link to="/view" class='box-text'>Coffee Budget</router-link>
         </div>
-        
         <div class='box'>
           <router-link to="/add" class='box-text'>Create new</router-link>
         </div>
-        
       </div>
       
     </div>
     <div class='section'>
       <h1>Collections</h1>
       <router-link to="/template">Template</router-link>
+
+      <div class='box-container'>
+        <div class='box'>
+          <router-link to="/view" class='box-text'>Food</router-link>
+        </div>
+        <div class='box'>
+          <router-link to="/add" class='box-text'>Create new</router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -56,6 +42,7 @@ export default {
   name: 'HomeScreen'
 }
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -13,3 +13,6 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
+
